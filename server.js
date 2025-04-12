2a14aa70d5f0ca0e5a050a97a83c0c2099fc0b69
Nino678190 <superwauzi@gmx.de>
2025-04-12 14:49:50 +0200

    Test

--- server.js
+++ server.js
@@ -2,30 +2,32 @@ const express = require('express');
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({ host: process.env.DB_HOST, user: process.env.DB_USER, connectionLimit: 5 });
 const app = express();
+const bcrypt = require('bcrypt');
 app.use(express.json())
+const saltRounds = 10
+function hash(password) {
+    return bcrypt.hash(password, saltRounds);
+}
 
 
+//Bcrypt importieren
+//Hash Funktion zum laufen kriegen
+//Registierungesendpoint mit Post methode
+//Username und Passwort akzeptieren
+//Passwort hashen
+//Username + Passwort -> DB
+//201 Code mit Created
 
-
-
-//POST Anfrage
-//Email-Adresse bekommen (keine KOntrolle)
-//In Datenbank speichern
-//201 mit hinzugefügt
-
-
-app.post('/', async (req,res)=>{
-    let {email} = req.body
+app.post('/api/registration', async (req,res)=>{
+    let {username,password} = req.body
     let conn;
     try {
-
         conn = await pool.getConnection();
 
-        const res = await conn.query("INSERT INTO myTable value (?)", [email]);
-        // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+        const res = await conn.query("INSERT INTO myTable value (?,?)", [username,hash(password)]);
 
     } 
-    catch(error){
+    catch (error) {
         res.status(500).send("server error ",error)
         return;
     }
@@ -33,7 +35,7 @@ app.post('/', async (req,res)=>{
     finally {
         if (conn) conn.release(); //release to pool
     }
-    res.status(201).send("hinzugefügt")
+    res.status(201).send("Created")
 })
 
 
