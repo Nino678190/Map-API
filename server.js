8adf9470e62ec4382ea47ff5d9ccc0002b0eda4a
Nino678190 <superwauzi@gmx.de>
2025-04-12 16:59:38 +0200

    Interactions and seights

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ function hash(password) {
     return bcrypt.hash(password, saltRounds);
 }
 
-
 //Bcrypt importieren
 //Hash Funktion zum laufen kriegen
 //Registierungesendpoint mit Post methode
@@ -18,25 +17,106 @@ function hash(password) {
 //Username + Passwort -> DB
 //201 Code mit Created
 
-app.post('/api/registration', async (req,res)=>{
-    let {username,password} = req.body
+app.post('/api/registration', async (req, res) => {
+    let { username, password } = req.body
     let conn;
     try {
         conn = await pool.getConnection();
 
-        const res = await conn.query("INSERT INTO myTable value (?,?)", [username,hash(password)]);
+        const res = await conn.query("INSERT INTO User value (?,?)", [username, hash(password)]);
 
-    } 
+    }
     catch (error) {
-        res.status(500).send("server error ",error)
+        res.status(500).send("server error ", error)
         return;
     }
-    
+
     finally {
         if (conn) conn.release(); //release to pool
     }
     res.status(201).send("Created")
 })
+
+app.post('/api/addseight', async (req, res) => {
+    let { name , xcoordinate , ycoordinate , type , price, description , opening_times} = req.body;
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const res = await conn.query("INSERT INTO User value (?,?,?,?,?,?,?)", [name, xcoordinate, ycoordinate, type, price, description, opening_times]);
+
+    }
+    catch (error) {
+        res.status(500).send("server error ", error)
+        return;
+    }
+
+    finally {
+        if (conn) conn.release(); //release to pool
+    }
+    res.status(201).send("Created")
+})
+
+
+///api/getsight/:id
+
+app.post('/api/login', async (req, res) => {
+    let { username, password } = req.body
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const db_getdata = await conn.query("SELECT username , password FROM User where username = (?)", [username]);
+        if (db_getdata){
+            const check = bcrypt.compare(password, db_getdata.password);
+            if (!check) {
+                return res.status(404).send("Nutzer nicht gefunden");
+            }
+        }    
+    } catch (error) {
+        res.status(500).send("server error ", error)
+        return;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+    res.status(201).send("Created")
+})
+
+app.post('/api/getsight/:id', async (req, res) => {
+    let { id } = req.params
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const db_getdata = await conn.query("SELECT * FROM User where id = (?)", [id]);
+        res.status(200).send(db_getdata)
+    } catch (error) {
+        res.status(500).send("server error ", error)
+        return;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+    
+})
+
+app.post('/api/allinteraction/:id',async (req, res) => {
+    let {id} = req.params
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const db_getdata = await conn.query("SELECT * FROM User where id = (?) and type = true", [id]);
+        anzahl_likes = db_getdata.rowCount
+        const db_getdata_negative = await conn.query("SELECT * FROM User where id = (?) and type = false", [id]);
+        anzahl_dislikes = db_getdata_negative.rowCount
+        res.status(200).send(anzahl_likes , anzahl_dislikes)
+    } catch (error) {
+        res.status(500).send("server error ", error)
+        return;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+
+}
+
+)
 
 
 app.listen(3000, () => {
